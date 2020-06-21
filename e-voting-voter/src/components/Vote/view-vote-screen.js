aa74956102fe09a901272a1f04fd1398889c3261
Thong Truong <thong1411998@gmail.com>
2020-06-21 21:30:57 +0700

    - get posting api

--- e-voting-voter/src/components/Vote/view-vote-screen.js
+++ e-voting-voter/src/components/Vote/view-vote-screen.js
@@ -4,8 +4,17 @@ import {elections} from './election-data';
 import {transactions} from '../Blockchain/Transaction/transaction-data';
 import {account} from '../Authentication/account-data';
 import VoteList from './vote-list';
+import Axios from 'axios';
 
 export default function  ViewVoteScreen({navigation}){
+
+	const voteDatas = () =>{
+		Axios.get('http://10.0.2.2:3000/posting')
+		.then(response => {
+			console.log('vote datas:',response.data);
+		})
+		.catch(error => console.log(error));
+	}
 
 	const myTransaction = transactions.filter(trans => trans.voterPublicKey === account.publicKey);
 	const cloneVotes = elections.filter(vote => vote.status === 1 || vote.status === 2);
